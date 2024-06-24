f5c8fb0ed6e04af8e72b3a29d8acee3c007a127a
Yasin Okunakol <yasinokunakol@gmail.com>
2024-06-24 12:06:14 +0200

    project created

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-6">
+      <Button >our Button</Button>
+    </main>
+  );
+}
