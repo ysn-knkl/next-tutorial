81f0fd5eabda6a2918f7c84e15d87e560a53af46
Yasin Okunakol <yasinokunakol@gmail.com>
2024-06-25 13:19:09 +0200

    global css arranged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="container mx-auto py-6">
-      <Button >our Button</Button>
+    <main className="py-2 container">
+      <Button variant="destructive" className="p-4">Our Button</Button>
     </main>
   );
 }
