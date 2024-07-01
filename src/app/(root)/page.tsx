dba2bd46b8219420b886f9ba7ee0590be6881689
Yasin Okunakol <yasinokunakol@gmail.com>
2024-07-01 12:04:58 +0200

    project loyout added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="py-2 container">
-      <Button variant="destructive" className="p-4">Our Button</Button>
+      <h1></h1>
     </main>
   );
 }
