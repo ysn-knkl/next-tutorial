cd368d5cbdad5f3a0e3fa1730b61c75a493fcd48
Yasin Okunakol <yasinokunakol@gmail.com>
2024-07-19 18:34:24 +0200

    form created and input, dropdown,fileuploader added

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,116 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "../ui/textarea"
+
+import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import { eventDefaultValues } from "@/constans"
+import Dropdown from "./Dropdown"
+import { useState } from "react"
+import { FileUploader } from "./FileUploader"
+
+type EventFormProps = {
+    userId: string
+    type: "Create" | "Update"
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+    const [files, setFiles] = useState<File[]>([])
+
+    const initialValues = eventDefaultValues
+
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+        resolver: zodResolver(eventFormSchema),
+        defaultValues: initialValues,
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof eventFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Input placeholder="Event title" {...field} className="input-field" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <Textarea placeholder="Description" {...field} className="textarea rounded-2xl" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <FileUploader
+                                        onFieldChange={field.onChange}
+                                        imageUrl={field.value}
+                                        setFiles={setFiles}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <Button type="submit">Submit</Button>
+
+            </form>
+        </Form>
+    )
+}
+
+export default EventForm
